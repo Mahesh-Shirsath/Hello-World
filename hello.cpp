aa7c3356a18f73f49087268cb8030de37f21f183
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-04 15:20:02 +0530

    We write the code for Hello world in c++.

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+//Here we are goingh to print Hello world using class
+#include<iostream.h>//header file
+class Hello
+{
+    int function()
+    {
+        cout<<"Hello world"<<endl;
+    }
+}
+int main()
+{
+    Hello h1;   //Here we create one object of class Hello
+    h1.function();  //Here we call the function
+}
