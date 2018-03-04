24fa205fa6766313e05018f121d15ea2123f7165
Mahesh-Shirsath <maheshshirsath106@gmail.com>
2018-03-04 15:20:48 +0530

    We write the code for Hello world in Java.

---
+++ hello.java
@@ -0,0 +1,9 @@
+/*Here we are going to write code in java using OOP(object
+oriented programming)*/
+public class hello //One thing always remember class name and file name must be same
+{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World")
+    }
+}
